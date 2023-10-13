476a817f9efaa5ae2e0c23960c5bf9f8e1292c40
sakanachqn <sakanachqn@gmail.com>
2023-10-13 12:23:51 +0900

    <Add>移動処理、カメラ回転基盤

--- Assets/Scripts/Kikuchi/ObjectCheck.cs
+++ Assets/Scripts/Kikuchi/ObjectCheck.cs
@@ -2,14 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class ObjectCheck : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
