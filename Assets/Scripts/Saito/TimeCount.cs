5418a19c55948b88457d7d98c99629021e2f6b7f
AKANE SAITO <TTC22C1020021@edu.tech.ac.jp>
2023-10-20 17:28:49 +0900

    TimeCount

--- Assets/Scripts/Saito/TimeCount.cs
+++ Assets/Scripts/Saito/TimeCount.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class TimeCount : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public int CountDownMinutes = 5;
+    private float countdownSeconds;
+    private TMP_Text timeText;
+
     void Start()
     {
-        
+        timeText = GetComponent<TMP_Text>();
+        countdownSeconds = CountDownMinutes * 60;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        countdownSeconds -= Time.deltaTime;
+       TimeSpan timeSpan = TimeSpan.FromSeconds(countdownSeconds);
+        timeText.text = timeSpan.ToString(@"mm\:ss");
+        Debug.Log(countdownSeconds);
+        if(countdownSeconds <= 0)//0秒になったときの処理
+        {
+            //ゲームオーバーの処理
+        }
     }
 }
