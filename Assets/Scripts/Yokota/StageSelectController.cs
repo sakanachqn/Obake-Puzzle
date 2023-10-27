abaae7aadcfa52618cf85746e176cdad71631682
YusakuYokota <ttc22c1020051@edu.tech.ac.jp>
2023-10-27 09:44:40 +0900

    ステージセレクト作成途中、CSVファイルプレイヤー読み込み改善

--- Assets/Scripts/Yokota/StageSelectController.cs
+++ Assets/Scripts/Yokota/StageSelectController.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class StageSelectController : MonoBehaviour
 {
-    //デッドゾーン設定用変数
-    [SerializeField]
-    private float deadZone = 0.5f;
-
     private ControllerInput inp;
 
     private Vector2 stickInclination;
@@ -32,37 +28,19 @@ public class StageSelectController : MonoBehaviour
 
     private void Update()
     {
-        stickInclination = inp.StageSelect.CursorMove.ReadValue<Vector2>();
+        if (inp.StageSelect.Back.WasPressedThisFrame())
+        {
+            // タイトルに戻る
+        }
 
-        //正規化
-        stickInclination = DeadZone(stickInclination);
+        if (inp.StageSelect.Tutorial.WasPressedThisFrame())
+        {
+            // チュートリアルに移る
+        }
 
-        if (stickInclination == Vector2.zero) stickDirection = Direction.Null;
-        else if (stickInclination.x == -1) stickDirection = Direction.Left;
-        else if (stickInclination.x == 1) stickDirection = Direction.Right;
-        else if (stickInclination.y == 1) stickDirection = Direction.Up;
-        else if (stickInclination.y == -1) stickDirection = Direction.Down;
-    }
-
-    private Vector2 DeadZone(Vector2 vec2)
-    {
-        vec2.x = DeadZoneCheck(vec2.x);
-        vec2.y = DeadZoneCheck(vec2.y);
-        return vec2;
-    }
-
-    /// <summary>
-    /// 引数が絶対値0.5以下かどうか
-    /// </summary>
-    /// <param name="value"></param>
-    /// <returns>0.5未満なら0 / 0.5以上なら1 or -1</returns>
-    private int DeadZoneCheck(float value)
-    {
-        bool minus = false;
-        if (value < 0) minus = true;
-        float abs = Mathf.Abs(value);
-        if (abs <= deadZone) return 0;
-        else if (minus) return -1;
-        else return 1;
+        if (inp.StageSelect.Decision.WasPressedThisFrame())
+        {
+            // メインゲームに移る
+        }
     }
 }
