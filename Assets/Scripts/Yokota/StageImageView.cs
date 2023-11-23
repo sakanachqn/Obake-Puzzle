7927f3b5c8143e2832a945299c30d7ef9846ddb4
YusakuYokota <ttc22c1020051@edu.tech.ac.jp>
2023-11-23 15:05:13 +0900

    コメント追加

--- Assets/Scripts/Yokota/StageImageView.cs
+++ Assets/Scripts/Yokota/StageImageView.cs
@@ -5,35 +5,46 @@ using UnityEngine.UI;
 
 public class StageImageView : MonoBehaviour
 {
+    // 今このイメージにカーソルがあっているか判定する変数
     public bool matchCursor = false;
 
+    // タイムライン
     private float timeLine = 0f;
 
     private Image image;
 
     private void Start()
     {
+        // 自分のイメージコンポーネントを取得
         image = gameObject.GetComponent<Image>();
     }
 
     private void Update()
     {
+        // カーソルがあっているとき
         if (matchCursor)
         {
+            // タイムラインを進める
             timeLine += Time.deltaTime;
             
+            // イメージのカラーを白にする
             image.color = Color.white;
 
+            // スケールを周期的に拡縮する
             gameObject.transform.localScale
                 = Vector3.one 
                 + new Vector3(Mathf.Abs(Mathf.Sin(timeLine * 3) / 10)
                              , Mathf.Abs(Mathf.Sin(timeLine * 3) / 10)
                              , 0);
         }
+        // カーソルがあっていないとき
         else
         {
+            // タイムラインを初期化
             timeLine = 0f;
+            // イメージをグレーにする
             image.color = Color.gray;
+            // 大きさを初期化
             gameObject.transform.localScale = Vector3.one;
         }
     }
--- Assets/Scripts/Yokota/StageSelectController.cs
+++ Assets/Scripts/Yokota/StageSelectController.cs
@@ -12,11 +12,11 @@ public class StageSelectController : MonoBehaviour
     // 今どのイメージににカーソルがあっているか
     private int nowCursorPos = 0;
 
-    [SerializeField]
     private StageSelectView stageSelectView;
 
     private SceneFade sceneFade;
 
+    // スティックの傾きを保存する変数
     private Vector2 stickInclination;
 
     private void Start()
@@ -25,12 +25,15 @@ public class StageSelectController : MonoBehaviour
 
         stageSelectView = GameObject.Find("Board").GetComponent<StageSelectView>();
 
+        // 動作試験の際に適当に使ったものです。モック版のときはタイトルシーンから使っている
+        // イメージからシーンフェードのスクリプトをとってきてください
+        sceneFade = GameObject.Find("FadeImage").GetComponent<SceneFade>();
+
         inp.Enable();
     }
 
     private async void Update()
     {
-        // とりあえずキーボードで動くようにしてます。あとから変更します
         if (inp.StageSelect.Back.WasPressedThisFrame())
         {
             // タイトルに戻る
@@ -54,31 +57,10 @@ public class StageSelectController : MonoBehaviour
         stageSelectView.BrightUp(nowCursorPos);
     }
 
-    private bool CheckSpriteChange(int key)
-    {
-        if (key > 0)
-        {
-            if (nowSelectStage < 2 
-                || nowSelectStage > stageSelectView.StageNum - 5) return false;
-            else return true;
-        }
-        else
-        {
-            if (nowSelectStage > stageSelectView.StageNum - 3
-                || nowSelectStage < stageSelectView.StageNum - 4) return false;
-            else return true;
-        }
-    }
-
-    private void GameSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        var mapGenerater = GameObject.FindWithTag("MapGenerater").GetComponent<CSVMapGenerate>();
-
-        mapGenerater.LoadStageNum = nowSelectStage + 1;
-
-        SceneManager.sceneLoaded -= GameSceneLoaded;
-    }
-
+    /// <summary>
+    /// スティックが倒されたときに読まれる関数
+    /// </summary>
+    /// <param name="context"></param>
     public void OnNavigate(InputAction.CallbackContext context)
     {
         // performedコールバックだけをチェックする
@@ -87,29 +69,84 @@ public class StageSelectController : MonoBehaviour
         // スティックの2軸入力取得
         stickInclination = context.ReadValue<Vector2>();
 
+        // スティックが左に傾いていて、カーソルが左端にないとき
         if (stickInclination.x == -1 && nowSelectStage > 1)
         {
+            // key:左が入力されたときに-1、右が入力されたときに1を設定する
             int key = -1;
+            // イメージのスプライトを変える必要があれば変更する
             if (CheckSpriteChange(key)) stageSelectView.SpriteChange(key);
+            // 必要がなければカーソルの位置を左へ動かす
             else nowCursorPos -= 2;
+            // 今選ぼうとしているステージ番号を更新する
             nowSelectStage -= 2;
         }
+        // スティックが右に傾いていて、カーソルが右端にないとき
         if (stickInclination.x == 1 && nowSelectStage < stageSelectView.StageNum - 2)
         {
             int key = 1;
+            // イメージのスプライトを変える必要があれば変更する
             if (CheckSpriteChange(key)) stageSelectView.SpriteChange(key);
+            // 必要がなければカーソルの位置を右へ動かす
             else nowCursorPos += 2;
+            // 今選ぼうとしているステージ番号を更新する
             nowSelectStage += 2;
         }
+        // スティックが上に傾いていて、カーソルが上側にないとき
         if (stickInclination.y == 1 && nowSelectStage % 2 != 0)
         {
+            // カーソルの位置を上へ動かす
             nowCursorPos--;
+            // 今選ぼうとしているステージ番号を更新する
             nowSelectStage--;
         }
+        // スティックが下に傾いていて、カーソルが上側にあるとき
         if (stickInclination.y == -1 && nowSelectStage % 2 == 0)
         {
+            // カーソルの位置を上へ動かす
             nowCursorPos++;
+            // 今選ぼうとしているステージ番号を更新する
             nowSelectStage++;
         }
+    }
+
+    /// <summary>
+    /// スティック操作があったときにイメージのスプライトを変える必要があるか判定する関数
+    /// </summary>
+    /// <param name="key">入力が右、左のどちらか判定する変数。右: 1、左: -1 </param>
+    /// <returns></returns>
+    private bool CheckSpriteChange(int key)
+    {
+        if (key > 0)
+        {
+            // カーソルが左端、または右端から2番目より右にある時
+            if (nowSelectStage < 2
+                || nowSelectStage > stageSelectView.StageNum - 5) return false; // 変更しない
+            else return true;   // 変更する
+        }
+        else
+        {
+            // カーソルが右端、または左端から2番目より左にある時
+            if (nowSelectStage > stageSelectView.StageNum - 3
+                || nowSelectStage < stageSelectView.StageNum - 4) return false; // 変更しない
+            else return true;   // 変更する
+        }
+    }
+
+    /// <summary>
+    /// ゲームシーンへ遷移するときに今何番目のステージを選択したか知らせる関数
+    /// </summary>
+    /// <param name="scene"></param>
+    /// <param name="mode"></param>
+    private void GameSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // マップを生成するスクリプトを探す
+        var mapGenerater = GameObject.FindWithTag("MapGenerater").GetComponent<CSVMapGenerate>();
+
+        // 読み込むステージの番号を書き換える
+        mapGenerater.LoadStageNum = nowSelectStage + 1;
+
+        // この処理を削除する
+        SceneManager.sceneLoaded -= GameSceneLoaded;
     }
 }
