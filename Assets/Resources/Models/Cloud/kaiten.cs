a1d72a8de4d9611061c09e2c3472e8788f3ec5eb
sakanachqn <sakanachqn@gmail.com>
2023-11-24 17:58:21 +0900

    commit漏れ

---
+++ Assets/Resources/Models/Cloud/kaiten.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Cysharp.Threading.Tasks;
+
+public class kaiten : MonoBehaviour
+{
+    private void Update()
+    {
+        this.transform.Rotate(new Vector3());
+    }
+}
