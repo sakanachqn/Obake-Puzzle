3a5673511fae0b06e7f89b488e7f898b2e795d61
nakamura natsuko <107166418+nakamuranatsuko@users.noreply.github.com>
2023-12-08 18:27:52 +0900

    タイトル画面関係_途中

--- Assets/Scripts/Nakamura/Title/TitleGimmickManager.cs
+++ Assets/Scripts/Nakamura/Title/TitleGimmickManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class TitleGimmickManager : MonoBehaviour
 {
@@ -12,7 +13,7 @@ public class TitleGimmickManager : MonoBehaviour
     private GameObject obake;
 
     [SerializeField]
-    private GameObject StartBtn;
+    private Image startBtn;
 
     [SerializeField]
     private int maxObakeCount = 30;
@@ -23,11 +24,30 @@ public class TitleGimmickManager : MonoBehaviour
     [SerializeField]
     private string GotoMapSelect;
 
-
     private Vector3 obakePosition;
+
+    [SerializeField]
+    private float width = 10.0f;
+
+    private Vector3 startBtnPos;
+
+    [SerializeField]
+    private GameObject bowl;
+
+    [SerializeField]
+    private List<Material> bowlMaterials = new List<Material>();
 
     private void Start()
     {
+        obakePosition = new Vector3(Random.Range(0f, 8.0f)
+                                , Random.Range(2.62f, 6.52f)
+                                , Random.Range(-0.25f, -2.0f));
+
+        startBtnPos = startBtn.transform.position;
+
+        //受け皿のマテリアルをランダムに変更
+        int index = Random.Range(0,bowlMaterials.Count);
+        bowl.GetComponent<MeshRenderer>().material = bowlMaterials[index];
     }
 
     /// <summary>
@@ -36,10 +56,21 @@ public class TitleGimmickManager : MonoBehaviour
     /// </summary>
     private async void Update()
     {
+        //ボタンを揺らす
+        float sin = Mathf.Sin(Time.time) * width;
+        startBtn.transform.position = new Vector3(startBtnPos.x + sin, startBtnPos.y, startBtnPos.z);
+
+
         // ボタンが押されたら画面遷移する
         if (ControllerManager.instance.CtrlInput.TitleGimmick.SelectMap.WasPressedThisFrame())
         {
+            //少し拡大する
+            await startBtn.transform.DOScale(new Vector3(startBtn.gameObject.transform.position.x + 2.0f
+                                                        , startBtn.gameObject.transform.position.y + 2.0f
+                                                        , startBtn.gameObject.transform.position.z)
+                                                        , 1f);
 
+            await SceneFade.instance.SceneChange("GotoMapSelect");
         }
 
         //オバケが最大数超えたら
