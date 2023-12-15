c605e2fc34b095810542359e06f6e28eb0a4e6a3
nakamura natsuko <107166418+nakamuranatsuko@users.noreply.github.com>
2023-12-15 16:44:26 +0900

    スタートボタンギミック

--- Assets/Scripts/Nakamura/Title/TitleGimmickManager.cs
+++ Assets/Scripts/Nakamura/Title/TitleGimmickManager.cs
@@ -28,6 +28,8 @@ public class TitleGimmickManager : MonoBehaviour
 
     [SerializeField]
     private float width = 10.0f;
+    [SerializeField]
+    private float jumpPower = 100f;
 
     private Vector3 startBtnPos;
 
@@ -39,15 +41,16 @@ public class TitleGimmickManager : MonoBehaviour
 
     private void Start()
     {
-        obakePosition = new Vector3(Random.Range(0f, 8.0f)
-                                , Random.Range(2.62f, 6.52f)
-                                , Random.Range(-0.25f, -2.0f));
-
-        startBtnPos = startBtn.transform.position;
+        //スタートボタンの位置を保存
+        startBtnPos = startBtn.rectTransform.localPosition;
 
         //受け皿のマテリアルをランダムに変更
         int index = Random.Range(0,bowlMaterials.Count);
         bowl.GetComponent<MeshRenderer>().material = bowlMaterials[index];
+
+        //ボタンを揺らす(ループ)
+        var rect = startBtn.GetComponent<RectTransform>();
+        rect.DOJumpAnchorPos(new Vector2(width, startBtnPos.y), jumpPower, 1, 1,true).SetLoops(-1, LoopType.Yoyo);
     }
 
     /// <summary>
@@ -56,19 +59,11 @@ public class TitleGimmickManager : MonoBehaviour
     /// </summary>
     private async void Update()
     {
-        //ボタンを揺らす(横)
-        float sin = Mathf.Sin(Time.time) * width;
-        startBtn.transform.position = new Vector3(startBtnPos.x + sin, startBtnPos.y, startBtnPos.z);
-
-
         // ボタンが押されたら画面遷移する
         if (ControllerManager.instance.CtrlInput.TitleGimmick.SelectMap.WasPressedThisFrame())
         {
             //少し拡大する
-            await startBtn.transform.DOScale(new Vector3(startBtn.gameObject.transform.position.x + 2.0f
-                                                        , startBtn.gameObject.transform.position.y + 2.0f
-                                                        , startBtn.gameObject.transform.position.z)
-                                                        , 1f);
+            await startBtn.transform.DOScale(new Vector3(1.1f, 1.1f, 0), 1f);
 
             await SceneFade.instance.SceneChange("StageSelect");
         }
@@ -79,6 +74,9 @@ public class TitleGimmickManager : MonoBehaviour
         //ボタン押されたらオバケを生成する
         if (ControllerManager.instance.CtrlInput.TitleGimmick.Obake.WasPressedThisFrame())
         {
+            //ランダム位置
+            obakePosition = new Vector3(4.0f, Random.Range(2.6f, 5.0f), -0.86f);
+
             Instantiate(obake, obakePosition, Quaternion.Euler(0, 180, 0));
             obakeCount++;
         }
