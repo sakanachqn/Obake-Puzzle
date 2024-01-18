7209d128098231fb3f6632cfe4a13370d47a0b67
sakanachqn <sakanachqn@gmail.com>
2024-01-18 15:36:40 +0900

    <Add>アニメーション等マージ準備

--- Assets/Scripts/Kikuchi/InGame/Skill/SkillManager.cs
+++ Assets/Scripts/Kikuchi/InGame/Skill/SkillManager.cs
@@ -22,11 +22,13 @@ public class SkillManager : MonoBehaviour
     public static bool IsNowSkill = false;
 
     // 現在スキル発動位置選択中かどうか
-    private bool isPosSelectNow = false;
+    public bool isPosSelectNow = false;
 
     // 各スキルのクラスを用意
     private Skill currentSkillA;
     private Skill currentSkillB;
+
+    
 
     // スキルのタイプ
     private enum skillType
@@ -91,7 +93,7 @@ public class SkillManager : MonoBehaviour
             // コントローラーマネージャーのstickSkillDirectionがNullでない場合
             if (ControllerManager.instance.stickPlayerDirection != ControllerManager.Direction.Null && !isPosSelectNow)
             {
-                GetSkillPos();
+                skillArea.GetSkillPos();
             }
             if(ctrl.CtrlInput.Skill.Select.WasPressedThisFrame() && selectSkill == skillType.skillA)
             {
@@ -106,90 +108,7 @@ public class SkillManager : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// スキル発動位置を取得する非同期メソッド
-    /// </summary>
-    /// <returns></returns>
-    private async void GetSkillPos()
-    {
-        isPosSelectNow = true;
+    
 
-        var direcDic = plCon.plMove.Directions;
-
-        var pos = skillArea.posObj.transform.position;
-
-        // 方向に応じた処理を実行
-        if(CheckObject(pos, direcDic[ControllerManager.instance.stickPlayerDirection], out string name))
-        {
-            skillArea.posObj.transform.position += direcDic[ControllerManager.instance.stickPlayerDirection];
-            if(name == "Player")
-            {
-                skillArea.posObj.transform.position += direcDic[ControllerManager.instance.stickPlayerDirection];
-            }
-        }
-        else 
-        {
-            if (!CheckOffMap(direcDic[ControllerManager.instance.stickPlayerDirection]))
-            {
-                skillArea.posObj.transform.position = this.transform.position + direcDic[ControllerManager.instance.stickPlayerDirection];
-            }
-        }
-        await UniTask.Delay(250);
-        isPosSelectNow = false;
-    }
-
-    /// <summary>
-    /// 移動先にobjectがあるか確認するメソッド
-    /// </summary>
-    /// <param name="targetDirec">スティックの方角</param>
-    /// <returns>移動先にobjがあったらtrue</returns>
-    private bool CheckObject(Vector3 startPos, Vector3 targetDirec, out string name)
-    {
-        //移動したい方向にrayを飛ばしてオブジェクトがあるか確認
-        if (Physics.Raycast(startPos, targetDirec, out var hitObj, 1))
-        {
-            if (hitObj.collider.tag == "Pitfall")
-            {
-                name = null;
-                return false;
-            }
-            else if (hitObj.collider.tag == "clone")
-            {
-                name = null;
-                return true;
-            }
-            else if (hitObj.collider.tag == "Player")
-            {
-                name = hitObj.collider.tag;
-                return true;
-            }
-            else
-            {
-                name = null;
-                return false;
-            }
-        }
-        else
-        {
-            name = null;
-            return false; 
-        }
-    }
-
-    /// <summary>
-    /// マップ外かどうかのチェック
-    /// </summary>
-    /// <param name="targetDirec">スティックの方角</param>
-    /// <returns>オブジェクトがなかったらfalse</returns>
-    private bool CheckOffMap(Vector3 targetDirec)
-    {
-        Vector3 startPos = skillArea.posObj.transform.position + targetDirec;
-        //移動先から真下にray飛ばして地面があるか確認
-        if (CheckObject(startPos, Vector3.down, out var name))
-        {
-            return true;
-        }
-        else return false; ;
-    }
 
 }
