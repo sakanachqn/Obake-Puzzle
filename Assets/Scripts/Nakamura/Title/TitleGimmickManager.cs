cac2864034fbddcf1709359829afe5a4baee1a48
nakamura natsuko <107166418+nakamuranatsuko@users.noreply.github.com>
2024-02-02 10:29:08 +0900

    スキルUI差し替えスクリプト

--- Assets/Scripts/Nakamura/Title/TitleGimmickManager.cs
+++ Assets/Scripts/Nakamura/Title/TitleGimmickManager.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using UnityEngine.UI;
